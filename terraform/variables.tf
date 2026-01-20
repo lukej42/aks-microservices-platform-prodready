9200263a35003d71b58f8603a62ee5eeadaace5d
luke.gumbleton <luke.gumbleton@haymarket.com>
2026-01-20 19:47:05 +0000

    Initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "keyvaultname" {
+    type = string
+}
+variable "location" {
+    type = string
+}
+variable "resource_group_name" {
+    type = string
+}
+variable "tenant_id" {
+    type = string
+}
+variable "spobject_id" {
+    type = string
+}
+variable "subscription_id" {
+    type = string
+}
+variable "acr_name" {
+    type = string
+}
+variable "acr_sku" {
+    type = string
+}
